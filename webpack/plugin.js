59ad3ed2423f0e8c63303848723379682e628e7f
tianshengjie <tianshengjie@howzhi.com>
2018-03-07 00:00:49 +0800

    update

--- webpack/plugin.js
+++ webpack/plugin.js
@@ -5,26 +5,26 @@ const argv = require('yargs').argv;
 import setting from './setting.js';
 
 let plugin = [
-    new webpack.BannerPlugin(setting.author),
-    new webpack.HotModuleReplacementPlugin(),
-    new ExtractTextPlugin('[name].css', {
-        allChunks: true
-    },
-    new webpack.optimize.CommonsChunkPlugin('common'),
-    new webpack.ProvidePlugin({
-        $: 'jquery', 
-        jQuery: 'jquery', 
-        'window.jQuery': 'jquery',
-        'window.$': 'jquery',
-    }),
-)]
+	new webpack.BannerPlugin(setting.author),
+	new webpack.HotModuleReplacementPlugin(),
+	new ExtractTextPlugin('[name].css', {
+		allChunks: true
+	}),
+	new webpack.optimize.CommonsChunkPlugin('common'),
+	new webpack.ProvidePlugin({
+		$: 'jquery', 
+		jQuery: 'jquery', 
+		'window.jQuery': 'jquery',
+		'window.$': 'jquery',
+	}),
+];
 
 if ('prod' === argv.env) {
-    plugin.push(new webpack.optimize.UglifyJsPlugin({
-        compress: {
-        warnings: false
-        }
-    }));
+	plugin.push(new webpack.optimize.UglifyJsPlugin({
+		compress: {
+			warnings: false
+		}
+	}));
 }
 
 export default plugin;
