8b15800f6d4f93d9e474c21fbbd90258d66755d9
tianshengjie <tianshengjie@howzhi.com>
2018-05-08 09:23:32 +0800

    update static

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 require('./../less/app.less');
+$('body').addClass();
 // import 'bootstrap';
 // require('codeages-design/dist/codeages-design.js');
 // import 'codeages-design/dist/codeages-design.css';
