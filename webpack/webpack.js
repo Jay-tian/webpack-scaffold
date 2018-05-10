c5f7af8212b768a275335338b733d9156d65e02c
tianshengjie <tianshengjie@howzhi.com>
2018-05-10 23:31:07 +0800

    update webpack/webpack

--- webpack/webpack.js
+++ webpack/webpack.js
@@ -1,7 +1,6 @@
 const entries = require('./entries.js');
 const loader = require('./loader.js');
 const plugin = require('./plugin.js');
-const process = require('process');
 const config = require('./config.js');
 
 let webpackConfig = {
@@ -19,17 +18,5 @@ let webpackConfig = {
   },
   plugins: plugin,
 };
-
-if ('development' ===  config.env) {
-  webpackConfig.devtool = 'eval-source-map';
-  webpackConfig.devServer = {
-    contentBase: process.cwd(),
-    port: 3032,
-    inline: false,
-    historyApiFallback: false,
-    hot: true,
-    stats: 'normal',
-  };
-}
 
 module.exports = webpackConfig;
